749c6029590e314858619809fe414585b7e904a2
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-09-06 15:49:02 +0530

    allowed cors for live app

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 app.use(cors({
     origin: 'http://localhost:5173', 
-    origin:'https://dr-harish-kawatra.vercel.app/patients/ExistingPatient'// Adjust this to match your frontend URL
+    origin:'https://dr-harish-kawatra.vercel.app/'// Adjust this to match your frontend URL
 }));
 app.use(express.json());
 
