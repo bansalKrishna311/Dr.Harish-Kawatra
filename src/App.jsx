4cae08e778e70f0196c3cc215d8a34a5d03af389
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-09-12 15:15:10 +0530

    Refactor routes and sidebar NavLink to use root path instead of /home

--- src/App.jsx
+++ src/App.jsx
@@ -153,7 +153,7 @@ function App() {
             </DefaultLayout>
           }
         />
-        <Route
+        {/* <Route
           path="/"
           element={
             <LoginLayout>
@@ -170,7 +170,7 @@ function App() {
               <Signup />
             </LoginLayout>
           }
-        />
+        /> */}
 
         {/* Patients Routes */}
         <Route
